5d282080c4bc0b0ae9277d66fe8e52be571284b9
ROKMC1250 <hjh1037@gmail.com>
2025-10-22 20:07:15 +0900

    branch check

--- energy_train_remote_sensing.py
+++ energy_train_remote_sensing.py
@@ -546,7 +546,6 @@ def my_app(cfg: DictConfig) -> None:
 
             # step scheduler at end of epoch
             scheduler.step()
-            asdfasdf
 
             if epoch in eval_epochs:
                 model.eval()
